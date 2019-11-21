373ecf3b9f3b772e30e11aafe1d13109aa679f01
Pedro A. González <pgonzalez@post.com>
2019-11-21 01:17:41 -0500

    Limpieza, agregué comentarios y bonus

--- 3.5 Repaso/main.js
+++ 3.5 Repaso/main.js
@@ -1,8 +1,7 @@
-
+//Crear un arreglo con diferentes tipos de datos y mostrarlos en la consola uno por uno a través de un ciclo FOR
 
 // let array = [9, 30, 'a', 'hola', 10, 'a', 'hola', 10]
-
-
+//
 // for(let i = 0; i < array.length ; i = i + 1) {
 //   console.log( array[i] )
 // }
@@ -16,15 +15,29 @@
 // console.log(x)
 
 
-// for(let i = 1; i <= 100 ; i++) {
+//A través de un ciclo for mostrar en la consola los números del 1 al 100 e indicar si son par o impar
 
-//   if( i % 2 === 0){
-//     console.log(i, 'es par')
-//   }else {
-//     console.log(i, 'es impar')
-//   }
+//Bonus: agregar esto y los push para explicar ordenamiento en arreglos después de haber hecho el ejercicio
+// var pares = ["Pares:"]
+// var impares = ["Impares:"]
 
-// }
+for(let i = 1; i <= 100 ; i++) {
+
+  if( i % 2 === 0){
+    console.log(i, 'es par')
+    // pares.push(i)
+
+  }else {
+    console.log(i, 'es impar')
+    // impares.push(i)
+  }
+
+}
+
+//Mostrar en consola los arreglos pares e impares
+// console.log(pares)
+// console.log(impares)
+
 
 // Funciones
 
@@ -39,8 +52,11 @@ const resta = function(a, b){
 let x = suma(10, 6)
 let y = suma(x, 100)
 
+let sum = suma(suma(10, 6), 100)
 
-let res = suma(suma(10, 6), 100)
-console.log(y)
-console.log(res)
+let res = resta(sum, 100)
 
+console.log(y) //116
+console.log(sum) //116
+console.log(x) //16
+console.log(res) //16
